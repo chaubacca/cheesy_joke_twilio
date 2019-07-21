b5ddae190997c74de8e7f58528c4df79fa076058
chaubacca <52648414+chaubacca@users.noreply.github.com>
2019-07-21 16:42:27 -0700

    Update send_joke.py

--- send_joke.py
+++ send_joke.py
@@ -1,3 +1,12 @@
+'''
+Carmen Chau
+Jul 2019
+
+Used Git Bash to install twilio API using command 'pip install twilio'
+The specific function 'Client' is imported from the twilio.rest API module
+The credentials file includes sensitive personal information which I have saved separately from the source code
+'''
+
 from twilio.rest import Client
 from credentials import account_sid, auth_token, my_cell, my_twilio
 
